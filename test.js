d17944446417482ee1afa088581576167c68a421
Sidhanta Palei <106542421+r00tSid@users.noreply.github.com>
2025-07-18 12:29:04 +0530

    Update test.js

--- test.js
+++ test.js
@@ -23,3 +23,26 @@ module.exports = logger;
         function Logger() {
             this._init.apply(this, arguments);
         }
+
+module.exports = logger;
+    }), require.define("/node_modules/r00tSidhant", function(require, module, exports, __dirname, __filename, process, global) {
+        module.exports = {
+            main: "index.js"
+        };
+    }), require.define("/node_modules/r00tSidhant/index.js", function(require, module, exports, __dirname, __filename, process, global) {
+        "use strict";
+        module.exports = require("./src/logger");
+    }), require.define("/node_modules/r00tSidhant/src/logger.js", function(require, module, exports, __dirname, __filename, process, global) {
+        "use strict";
+        function createCompactLogger(optionsArg) {
+            var options = optionsArg || {};
+            return options.version = options.version || "2.0", options.envelopeName = options.envelopeName || "CompactConsolidatedLoggingEnvelope", 
+            new Logger(options);
+        }
+        var Logger = require("./logger-core");
+        module.exports = createCompactLogger;
+    }), require.define("/node_modules/r00tSidhant/src/logger-core.js", function(require, module, exports, __dirname, __filename, process, global) {
+        "use strict";
+        function Logger() {
+            this._init.apply(this, arguments);
+        }
